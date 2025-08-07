9088a45b2dab7a2846bef878e9d012375ddcbe85
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-08-07 19:27:52 -0300

    Feat: screen
    Add camera effect on screen

---
+++ src/screens/Garage/GarageScreen.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './Styles'
+
+const GarageScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cameraBox}>
+        <View style={styles.triangleCorner}></View>
+        <View style={[styles.triangleCorner, styles.topRight]}></View>
+        <View style={[styles.triangleCorner, styles.bottomRight]}></View>
+        <View style={[styles.triangleCorner, styles.bottomLeft]}></View>
+      </View>
+      <Text>GarageScreen</Text>
+    </View>
+  )
+}
+
+export default GarageScreen
