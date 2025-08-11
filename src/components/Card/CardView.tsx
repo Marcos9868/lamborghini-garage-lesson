4bc1a8a056cc82ceb5aa9d42697dbb8bc3ea60d5
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-08-11 19:10:04 -0300

    Feat: component

--- src/components/Card/CardView.tsx
+++ src/components/Card/CardView.tsx
@@ -4,15 +4,20 @@ import styles from './Styles'
 import '../../../index.d.ts'
 
 import logo from '../../../assets/logo.png';
+import Divider from '../Divider';
 
 const CardView = () => {
+  const renderLogoBox = () => (
+    <View style={styles.logoContainer}>
+      <Image style={styles.imageLogo} source={logo}/>
+    </View>
+  );
   return (
     <View style={styles.imageContainer}>
-      <View style={styles.logoContainer}>
-        <Image style={styles.imageLogo} source={logo} />
-      </View>
-    </View>
-  )
+      {renderLogoBox()}
+      <Divider />
+    </View> 
+  );
 }
 
 export default CardView
