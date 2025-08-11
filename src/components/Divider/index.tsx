5dc623da77cf3268be88ee53b16f7fa4237f7618
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-08-11 19:10:24 -0300

    Feat: component
    Add divider for card view

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './styles'
+
+const Divider = () => {
+  return (
+    <View style={styles.dividerContainer}>
+      <View style={styles.divider}></View>
+    </View>
+  )
+}
+
+export default Divider 
