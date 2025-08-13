e2e3152ea8cf2e9c34767670a70fd9ff7903780f
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-08-13 20:39:28 -0300

    Feat: actions
    Add load content data and handle previous or next data on card view

--- src/components/Card/Actions.ts
+++ src/components/Card/Actions.ts
@@ -1,3 +1,33 @@
-export const loadCarData = async () => {};
-export const handlePreviousItem = async () => {};
-export const handleNextItem = async () => {};
+import { getCarData } from "../../api/service/getCars";
+import { CarModel } from "./Props";
+
+export const loadCarData = async (id: number, setCarData: React.Dispatch<React.SetStateAction<CarModel | null>>) => {
+  const response = await getCarData(id);
+  try {
+    if (response) {
+      setCarData(response)
+    }
+  } catch (error) {
+    console.log(error) 
+  }
+};
+export const handlePreviousItem = async (carData: CarModel | null, setCarData: React.Dispatch<React.SetStateAction<CarModel | null>>) => {
+  try {
+    if (carData && carData?.id > 1) {
+      const response = await getCarData(carData.id - 1);
+      if (response) setCarData(response)
+    }
+  } catch (error) {
+    console.log(error)  
+  }
+};
+export const handleNextItem = async (carData: CarModel | null, setCarData: React.Dispatch<React.SetStateAction<CarModel | null>>) => {
+  try {
+    if (carData && carData?.id > 1) {
+      const response = await getCarData(carData.id + 1);
+      if (response) setCarData(response)
+    }
+  } catch (error) {
+    console.log(error)  
+  }
+};
