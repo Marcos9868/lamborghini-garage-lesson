884d103d73c6b6f2fbfcbbefa05d132bbdd5c34d
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-08-13 20:38:59 -0300

    Feat: api
    Add api endpoints to renderize content

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,1 +1,6 @@
+import axios from "axios"
+
 export const BASE_CAR_URL = "https://digitalinnovationone.github.io/fake-data-api-lamborghini/assets"
+export const BASE_URL_CAR_DATA = "https://digitalinnovationone.github.io/fake-data-api-lamborghini"
+
+export const api = axios.get(BASE_URL_CAR_DATA)
